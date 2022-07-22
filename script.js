64b1df4d23bdce94b8ba5a8b6da2e9fc9a6ab6e2
DannnLim <Dannylim-@hotmail.com>
2022-07-22 21:09:25 +1000

    done

--- script.js
+++ script.js
@@ -65,42 +65,14 @@ $(".Like-Unlike").click(function (e) {
 });
 
 
-// $(document).ready(function () {
-//     $.getJSON("db.json",
-//         function (data) {
-//             var Songs = ' ';
 
-//             $.each(data, function (key, value) {
-
-//                 Songs += '<tr>';
-
-//                 Songs += '<td>' +
-//                     value.HappyBirthday + '</td>';
-
-//                 // Songs += '<td>' +
-//                 //     value.TwinkleTwinkle + '<td>'
-
-//                 Songs += '</tr>';
-//             });
-//             $('#Notation').append(Songs);
-//         });
-// });
-
-
-
-// fetch('db.json')
-//     .then(res => res.json())
-//     .then(data => {
-//         console.log(data.SongNotation)
-//         document.querySelector('#Notation').innerText = JSON.stringify(data)
-//     })
 
 
 const btn = document.querySelector('#btn');
 const output = document.querySelector('#Notation');
 const url = 'db.json';
 btn.onclick = () => {
-    // output.innerHTML = 'conecting....';
+
     getData();
 
 }
@@ -124,102 +96,3 @@ function outData(val) {
     output.innerHTML = html;
 
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const Piano = new Howl({
-//     "src": [
-//         "./Piano-Samples/Piano.webm",
-//         "./Piano-Samples/Piano.mp3"
-//     ],
-//     "sprite": {
-//         "Ab": [
-//             0,
-//             750
-//         ],
-//         "A": [
-//             2000,
-//             750
-//         ],
-//         "Bb": [
-//             4000,
-//             750
-//         ],
-//         "B": [
-//             6000,
-//             750
-//         ],
-//         "C": [
-//             8000,
-//             750
-//         ],
-//         "C2": [
-//             10000,
-//             750
-//         ],
-//         "Db": [
-//             12000,
-//             750
-//         ],
-//         "D": [
-//             14000,
-//             750
-//         ],
-//         "Eb": [
-//             16000,
-//             750
-//         ],
-//         "E": [
-//             18000,
-//             739.7278911564626
-//         ],
-//         "F": [
-//             20000,
-//             750
-//         ],
-//         "Gb": [
-//             22000,
-//             750
-//         ],
-//         "G": [
-//             24000,
-//             750
-//         ]
-//     }
-// });
-
-// // Piano.play("A");
-
-
-// const keys = document.querySelector('.piano');
-
-// keys.addEventListener('click', () => {
-//     if (event.target.classList.contains('key')) {
-//         let soundToPLay = event.target.dataset.sound;
-//         keys.style.backgroundColor = "#333";
-//         Piano.play(soundToPLay);
-
-//     }
-// });
-
